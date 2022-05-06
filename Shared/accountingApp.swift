8772011a729764772a74f6de8bb6a94cf7406a1e
KrisPeng <kris19951112@gmail.com>
2022-05-06 17:07:26 +0800

    Initial Commit

---
+++ Shared/accountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountingApp.swift
+//  Shared
+//
+//  Created by Kris on 2022/5/6.
+//
+
+import SwiftUI
+
+@main
+struct accountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
